2fd3aaccb81e33dc88457d63330db061df547fb6
hunzzzzz <hun819.st@gmail.com>
2025-11-17 20:06:21 +0900

    프로젝트 초기 파일 업로드

---
+++ src/test/kotlin/com/moira/thebyte/TheByteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moira.thebyte
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheByteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
